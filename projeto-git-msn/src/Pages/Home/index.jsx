05bd476c387e631933fa2def653953255ac29df9
Gustavo071-Dev <luiz.g.cruz@ba.estudante.senai.br>
2026-06-09 20:33:03 -0300

    codigo page home

--- projeto-git-msn/src/Pages/Home/index.jsx
+++ projeto-git-msn/src/Pages/Home/index.jsx
@@ -1,1 +1,24 @@
 import './style.css'
+
+export default function Home() {
+    return(
+        <div className='page-home'>
+            <h1>Bem-vindo a página: Trio MSN ⚽</h1> 
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit possimus quasi dignissimos repudiandae excepturi distinctio modi sit repellat beatae sed labore architecto, numquam saepe quo necessitatibus in deleniti dolorem!
+            </p>
+
+            <ul>
+                <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, aperiam accusantium expedita, vitae officia hic ipsam a quod odit earum modi vero, nihil omnis excepturi totam aspernatur perferendis sequi recusandae?
+                </li>
+                <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, et natus nostrum id praesentium, earum, saepe corrupti autem impedit reprehenderit nihil dolorem voluptate. Numquam eaque architecto quisquam, facere odio perferendis.
+                </li>
+                <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magni maiores fugit molestiae doloribus ullam, modi, veniam minima sit mollitia atque facilis suscipit assumenda eum quam, deleniti qui possimus aperiam!
+                </li>
+            </ul>    
+        </div>
+    )
+}
