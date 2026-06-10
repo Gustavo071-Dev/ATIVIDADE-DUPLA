ab247581db4eada94b134757e43df5055ad8f818
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-09 21:13:08 -0300

    codigo app.jsx

--- projeto-git-msn/src/components/Header/index.jsx
+++ projeto-git-msn/src/components/Header/index.jsx
@@ -1,1 +1,27 @@
 import './style.css'
+
+import { Link, NavLink } from 'react-router-dom';
+
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <Link to="/" className="header-logo">
+                <h1>The GOATS</h1>
+                </Link>
+                </div>
+
+                <nav>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Messi</NavLink>
+                    <NavLink to="/">Neymar</NavLink>
+                    <NavLink to="/">Cristiano</NavLink>
+
+                </nav>
+
+                
+            
+        </header>
+    )
+}
