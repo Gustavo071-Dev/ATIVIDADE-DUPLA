8848f95f89f9523e45156fe741659e0d9b400adc
Gustavo071-Dev <luiz.g.cruz@ba.estudante.senai.br>
2026-06-09 21:08:18 -0300

    codigo neymar

--- projeto-git-msn/src/Pages/Home/index.jsx
+++ projeto-git-msn/src/Pages/Home/index.jsx
@@ -1,24 +1,46 @@
 import './style.css'
 
 export default function Home() {
-    return(
-        <div className='page-home'>
-            <h1>Bem-vindo a página: Trio MSN ⚽</h1> 
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit possimus quasi dignissimos repudiandae excepturi distinctio modi sit repellat beatae sed labore architecto, numquam saepe quo necessitatibus in deleniti dolorem!
-            </p>
+  return (
+    <div className='page-home'>
+      <header className="home-header">
+        <h1>O Lendário Trio MCN ⚽</h1>
+        <p className="subtitle">
+          Explore a história, as estatísticas e a genialidade de três dos maiores camisas 10 e 7 que o futebol já viu: Messi, Cristiano Ronaldo e Neymar.
+        </p>
+      </header>
 
-            <ul>
-                <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, aperiam accusantium expedita, vitae officia hic ipsam a quod odit earum modi vero, nihil omnis excepturi totam aspernatur perferendis sequi recusandae?
-                </li>
-                <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, et natus nostrum id praesentium, earum, saepe corrupti autem impedit reprehenderit nihil dolorem voluptate. Numquam eaque architecto quisquam, facere odio perferendis.
-                </li>
-                <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magni maiores fugit molestiae doloribus ullam, modi, veniam minima sit mollitia atque facilis suscipit assumenda eum quam, deleniti qui possimus aperiam!
-                </li>
-            </ul>    
-        </div>
-    )
+      <main className="players-container">
+        {/* Card do Messi */}
+        <section className="player-card messi">
+          <div className="card-badge">ARG</div>
+          <h2>Lionel Messi</h2>
+          <p>
+            A genialidade pura em forma de futebol. Conhecido por sua genialidade, dribles curtos milimétricos e uma coleção incomparável de Bolas de Ouro.
+          </p>
+          <button className="card-btn">Ver Detalhes</button>
+        </section>
+
+        {/* Card do CR7 */}
+        <section className="player-card cristiano">
+          <div className="card-badge">POR</div>
+          <h2>Cristiano Ronaldo</h2>
+          <p>
+            A máquina de gols e o sinônimo de obstinação. O maior artilheiro da história da Champions League, movido a recordes e alta performance.
+          </p>
+          <button className="card-btn">Ver Detalhes</button>
+        </section>
+
+        {/* Card do Neymar */}
+        <section className="player-card neymar">
+          <div className="card-badge">BRA</div>
+          <h2>Neymar Jr</h2>
+          <p>
+            A alegria do futebol brasileiro e o auge do Joga Bonito. Ousadia, improviso e uma habilidade única de transformar o campo em um espetáculo.
+          </p>
+          <button className="card-btn">Ver Detalhes</button>
+        </section>
+      </main>
+    </div>
+  )
 }
